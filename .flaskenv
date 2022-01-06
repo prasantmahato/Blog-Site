0f5071101ce4a80f3f9d88611d2d087a3cdb7d21
prasantmahato <prasantmahato33@gmail.com>
2022-01-06 16:25:29 +0530

    Test deploy with some css modifications

--- .flaskenv
+++ .flaskenv
@@ -1,5 +1,5 @@
 FLASK_APP=main.py
-FLASK_ENV=production
+FLASK_ENV=development
 
 MAIL_SERVER=localhost
 MAIL_PORT=8025
--- app/routes.py
+++ app/routes.py
@@ -196,7 +196,7 @@ def reset_password_request():
         user = User.query.filter_by(email=form.email.data).first()
         if user:
             send_password_reset_email(user)
-        flash('Check your email for the instructions to reset your password')
+        flash('Check your email for the instructions to reset your password. Also check the spam folder.!')
         return redirect(url_for('login'))
     return render_template('reset_password_request.html',
                            title='Reset Password', form=form)

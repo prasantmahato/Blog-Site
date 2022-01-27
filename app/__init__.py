3f7cd289edf32f936ae742f9c4ccaa4b865764f0
prasantmahato <prasantmahato33@gmail.com>
2022-01-27 10:40:08 +0530

    Postgres database added

--- app/__init__.py
+++ app/__init__.py
@@ -38,14 +38,18 @@ if not app.debug:
 
     # Setting up an log file, at level INFO, 
     # max file size is 10kb which is updated on rotational basis
+    if app.config['LOG_TO_STDOUT']:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+    else:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/blogsite.log', maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
     
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/blogsite.log', maxBytes=10240, backupCount=10)
-    file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%lineno)d]'))
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-
     app.logger.setLevel(logging.INFO)
     app.logger.info('Blogsite Web')
